dcdb022e58c70257002f5a9505c2e69b3df48343
Emmanuel Adelugba <adelugba.emma@gmail.com>
2023-06-17 22:16:48 +0100

    ...

--- src/views/salesApp/sales/preview/PreviewCard.js
+++ src/views/salesApp/sales/preview/PreviewCard.js
@@ -5,6 +5,7 @@ import moment from 'moment'
 const PreviewCard = ({ data }) => {
 	const renderTable = (products) => {
 		// products = process.env.NODE_ENV === 'production' ? JSON.parse(products) : products
+		products = JSON.parse(products)
 		return products.map((product) => {
 			return (
 				<tr key={product.id}>
