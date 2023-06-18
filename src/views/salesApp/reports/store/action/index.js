69e491b029a664e2987465eda190ba1de0a7d3d4
Emmanuel Adelugba <adelugba.emma@gmail.com>
2023-06-18 15:13:12 +0100

    ...

--- src/views/salesApp/reports/store/action/index.js
+++ src/views/salesApp/reports/store/action/index.js
@@ -7,9 +7,16 @@ export const getSalesReport = ({ startDate, endDate }) => {
 		const response = await apiRequest({ url: '/sales-report', method: 'POST', body }, dispatch)
 		if (response) {
 			if (response.data.data && response.data.status) {
+				const report = response.data.data.sales.map(sale => {
+					return {
+						...sale,
+						products: JSON.parse(sale.product)
+					}
+				})
+				console.log({report})
 				await dispatch({
 					type: 'GET_ALL_REPORT_DATA',
-					data: response.data.data,
+					data: report,
 				})
 			} else {
 				console.log(response.error)
